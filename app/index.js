68a669c903f6c7372b5cde4ed481439d26e65fae
RAMU CHINNI <ramu1523@gmail.com>
2018-09-22 17:09:54 -0500

    updated props

--- app/index.js
+++ app/index.js
@@ -7,6 +7,31 @@ var USER_DATA = {
   username: 'tylermcginnis',
   img:'https://avatars0.githubusercontent.com/u/2933430?v=3&s=460'
 }
+class Avatar extends React.Component
+{
+  render(){
+    return(
+      <img src={this.props.img} />
+    )
+  }
+}
+class Label extends React.Component{
+  render()
+  {
+    return(
+      <h1> Name: {this.props.label} </h1>
+    )
+  }
+}
+
+class ScreenName extends React.Component{
+  render()
+  {
+    return(
+      <h3> username: {this.props.username} </h3>
+    )
+  }
+}
 class Badge extends React.Component
 {
   render()
@@ -14,9 +39,9 @@ class Badge extends React.Component
     console.log(this.props);
     return(
       <div>
-        <img src={this.props.user.img}/>
-        <h1>Name:{this.props.user.name} </h1>
-        <h3>username:{this.props.user.username}</h3>
+        <Avatar img={this.props.user.img}/>
+        <Label label={this.props.user.name}/>
+        <ScreenName username={this.props.user.username}/>
       </div>
     )
   }
