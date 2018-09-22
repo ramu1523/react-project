b6b115323eb7c3c9755376783db1b628df3f3880
RAMU CHINNI <ramu1523@gmail.com>
2018-09-22 16:40:26 -0500

    updated props

--- app/index.js
+++ app/index.js
@@ -2,7 +2,11 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 require('./index.css');
 //Component has State, life cycle methods/events and UI
-
+var USER_DATA = {
+  name: 'Tyler Mcginnis',
+  username: 'tylermcginnis',
+  img:'https://avatars0.githubusercontent.com/u/2933430?v=3&s=460'
+}
 class Badge extends React.Component
 {
   render()
@@ -10,17 +14,15 @@ class Badge extends React.Component
     console.log(this.props);
     return(
       <div>
-        <img src={this.props.img}/>
-        <h1>Name:{this.props.name} </h1>
-        <h3>username:{this.props.username}</h3>
+        <img src={this.props.user.img}/>
+        <h1>Name:{this.props.user.name} </h1>
+        <h3>username:{this.props.user.username}</h3>
       </div>
     )
   }
 }
 
 ReactDOM.render(
-  <Badge name='Tyler Mcginnis'
-  username='tylermcginnis'
-  img='https://avatars0.githubusercontent.com/u/2933430?v=3&s=460'/>,
+  <Badge user={USER_DATA}/>,
   document.getElementById('app')
 )
